1c40fe918d59006d05eca53487a5b22148745141
stoat2010 <stoat2010@mail.ru>
2018-07-31 15:18:37 +0300

    Переключаюсь на год

--- server/api/controllers/printouts.js
+++ server/api/controllers/printouts.js
@@ -36,22 +36,28 @@ module.exports.dataDevice = function(req, res) {
 };
 
 module.exports.dataGraph = function(req, res) {
-    var arr02=[0,0,0,0,0,0,0,0,0,0,0,0];
-    var prouts = [];
+
+    var prouts = [0];
+    
     Loc
-    .find({device: req.params.deviceid},{printouts: 1, month: 1,  _id: 0}).sort({date: 1}).limit(12)
+    .find({device: req.params.deviceid, year: new Date().getFullYear()},{printouts: 1, month: 1,  _id: 0}).sort({date: 1}).limit(12)
     .exec(function(err, device){
         if(err){
             sendJSONResponse(res, 440, err);
         }else{
-            var arr01 = device.map(item => item.printouts);
             var months = device.map(item=> item.month);
             var maxMonth = Math.max.apply(null, months);
-            var arr03 = arr02.concat(arr01);
-            var arr04 = arr03.slice((maxMonth + 1), arr03.length);
+        
+            var arr01 = device.map(item => item.printouts);
+            var k = maxMonth-arr01.length;
+
+            for (var j=0; j< k; j++){
+                var t = arr01.unshift(0);
+            };
+
             for( var i=1; i<=maxMonth; i++ ){
-                var val = arr04[i]-arr04[i-1];
-                prouts.push(val);
+                var val = arr01[i]-arr01[i-1];
+                prouts.push(val); 
             }
             sendJSONResponse(res, 200,prouts);
         }
