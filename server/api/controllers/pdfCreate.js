aaac7c698974f587fac12813e45e3acb71351394
stoat2010 <stoat2010@mail.ru>
2018-08-21 15:31:53 +0300

    4

--- server/api/controllers/pdfCreate.js
+++ server/api/controllers/pdfCreate.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('device');
 var LocPrint = mongoose.model('printout');
-var Chart = require('chart.js');
 
 module.exports.pdfCreate = async function (req, res) {
 
     const PDFDocument = require('pdfkit');
     var devices = await getDev();
-    
+
     let filename = 'report.pdf'
 
     res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
@@ -22,13 +21,13 @@ module.exports.pdfCreate = async function (req, res) {
     doc.font('Header Font').fontSize(25)
 
     var headerText = "Отчет по состоянию сетевой печатающей техники на: " + new Date().toLocaleDateString();
-    var x = doc.x+40, y = doc.y, h = 300, w = 400;
-    var optionsHeader = {width: w, align: 'center'};
-    doc.rect(x,y,w,h).stroke();
+    var x = doc.x + 40, y = doc.y, h = 300, w = 400;
+    var optionsHeader = { width: w, align: 'center' };
+    doc.rect(x, y, w, h).stroke();
 
-    doc.text(headerText, x , y + 0.5*(h-doc.heightOfString(headerText,optionsHeader)), optionsHeader);
+    doc.text(headerText, x, y + 0.5 * (h - doc.heightOfString(headerText, optionsHeader)), optionsHeader);
 
-    const results = devices.map( async (device)  => {
+    const results = devices.map(async (device) => {
 
         var balance = await balanceLoad(device.device);
         var dtGraph = await dataGraph(device.device, balance);
@@ -37,115 +36,111 @@ module.exports.pdfCreate = async function (req, res) {
         var dev1 = JSON.parse(JSON.stringify(device));
         dev1.dtgraph = dtGraph;
         dev1.daygraph = dayGraph;
-        
+
         return dev1;
     });
 
     await Promise.all(results).then(dev => {
 
+        dev.map(dev => {
 
-
-        dev.map( dev  => {
-
-        var xxx = 60;
-        var grData = dev.dtgraph;
-        var nans = grData.pop()
-        var dtgraphMax = Math.max(...grData);
-
-        var grDay = dev.daygraph;
-        var nans1 = grDay.pop()
-        var daygraphMax = Math.max(...grData);
-
-        doc.addPage()
-            .font('Header Font').fontSize(25)
-            .text(dev.model, 100, 100)
-            .fontSize(16)
-            .text("Отпечатки помесячно", 230, 340)
-            .moveTo(10,130).lineTo(600,130).lineWidth(5).stroke()
-            .moveTo(10,730).lineTo(600,730).lineWidth(5).stroke()
-            .font('Device Font')
-            .fontSize(12)
-            .text("Данные устройства:",20,215)
-            .font('Device Font')
-            .fontSize(12)
-            .text("Подразделение: ",40,155)
-            .text("Корпус: ",40,170)
-            .text("Кабинет: ",40,185)
-            .text(dev.unit,300,155)
-            .text(dev.build,300,170)
-            .text(dev.office,300,185)
-            .moveTo(10,200).lineTo(600,200).lineWidth(1).stroke()
-            .text("Производитель:",40,240)
-            .text("Серийный номер:",40,255)
-            .text("Сетевое имя:",40,270)
-            .text("IP:",40,285)
-            .text("Принят на обслуживание:",40,300)
-            .text(dev.vendor,300,240)
-            .text(dev.serial,300,255)
-            .text(dev.name,300,270)
-            .text(dev.device,300,285)
-            .text(new Date(dev.start_date).toLocaleDateString(),300,300)
-            .moveTo(10,315).lineTo(600,315).lineWidth(1).stroke()
-            .moveTo(55,385).lineTo(550,385).lineWidth(1).stroke()
-            .moveTo(45,652).lineTo(550,652).lineWidth(1).stroke()
-            .moveTo(50,657).lineTo(50,420).lineWidth(1).stroke()
-            .fontSize(10)
-            .text("  Янв",60, 370)
-            .text("  Фев",100, 370)
-            .text("  Мар",140, 370)
-            .text("  Апр",180, 370)
-            .text("  Май",220, 370)
-            .text("  Июн",260, 370)
-            .text("  Июл",300, 370)
-            .text("  Авг",340, 370)
-            .text("  Сен",380, 370)
-            .text("  Окт",420, 370)
-            .text("  Ноя",460, 370)
-            .text("  Дек",500, 370)
-            
-            grData.map(dtgr =>{
-                
-                if (isNaN(dtgr)){dtgr=0};
-                if (dtgr === 0){var dtgrprint=""}else{var dtgrprint=dtgr};
-                
-                if (dtgr <10){
-                    doc
-                        .fillColor("black")
-                        .text(dtgrprint, xxx+10, 390)
-                }else if(dtgr>10 & dtgr <= 999){
-                    doc
-                        .fillColor("black")
-                        .text(dtgrprint, xxx+8, 390)
-                }else{
-                    doc
-                        .fillColor("black")
-                        .text(dtgrprint, xxx+5, 390)
-                }
-
-                if (dtgr > 0){
-                    doc
-                        .rect(xxx+10, 450, 20, dtgr*200/dtgraphMax).fillAndStroke("#efebe9","#d7ccc8")
-                }else{
-                    doc
-                        .rect(xxx+10, 650, 20, 0).fillAndStroke("#efebe9","#d7ccc8")
-                }
-
-                xxx = xxx+ 40;
-                doc
-                    .moveTo(xxx-5,375).lineTo(xxx-5,395).lineWidth(1).stroke("black")
-                    .moveTo(xxx-20,652).lineTo(xxx-20,657).lineWidth(1).stroke("black")
-                    
-            }) 
             var xxx = 60;
-            grDay.map(dtgr =>{
-                
-                if (dtgr === 0){var dtgrprint=""}else{var dtgrprint= new Date(dtgr).toLocaleDateString()};
+            var grData = dev.dtgraph;
+            var nans = grData.pop()
+            var dtgraphMax = Math.max(...grData);
+            var grDay = dev.daygraph;
+            var nans1 = grDay.pop()
+
+            doc.addPage()
+                .font('Header Font').fontSize(25)
+                .text(dev.model, 100, 100)
+                .fontSize(16)
+                .text("Отпечатки помесячно", 230, 340)
+                .moveTo(10, 130).lineTo(600, 130).lineWidth(5).stroke()
+                .moveTo(10, 730).lineTo(600, 730).lineWidth(5).stroke()
+                .font('Device Font')
+                .fontSize(12)
+                .text("Данные устройства:", 20, 215)
+                .font('Device Font')
+                .fontSize(12)
+                .text("Подразделение: ", 40, 155)
+                .text("Корпус: ", 40, 170)
+                .text("Кабинет: ", 40, 185)
+                .text(dev.unit, 300, 155)
+                .text(dev.build, 300, 170)
+                .text(dev.office, 300, 185)
+                .moveTo(10, 200).lineTo(600, 200).lineWidth(1).stroke()
+                .text("Производитель:", 40, 240)
+                .text("Серийный номер:", 40, 255)
+                .text("Сетевое имя:", 40, 270)
+                .text("IP:", 40, 285)
+                .text("Принят на обслуживание:", 40, 300)
+                .text(dev.vendor, 300, 240)
+                .text(dev.serial, 300, 255)
+                .text(dev.name, 300, 270)
+                .text(dev.device, 300, 285)
+                .text(new Date(dev.start_date).toLocaleDateString(), 300, 300)
+                .moveTo(10, 315).lineTo(600, 315).lineWidth(1).stroke()
+                .moveTo(55, 385).lineTo(550, 385).lineWidth(1).stroke()
+                .moveTo(45, 652).lineTo(550, 652).lineWidth(1).stroke()
+                .moveTo(50, 657).lineTo(50, 420).lineWidth(1).stroke()
+                .fontSize(10)
+                .text("  Янв", 60, 370)
+                .text("  Фев", 100, 370)
+                .text("  Мар", 140, 370)
+                .text("  Апр", 180, 370)
+                .text("  Май", 220, 370)
+                .text("  Июн", 260, 370)
+                .text("  Июл", 300, 370)
+                .text("  Авг", 340, 370)
+                .text("  Сен", 380, 370)
+                .text("  Окт", 420, 370)
+                .text("  Ноя", 460, 370)
+                .text("  Дек", 500, 370)
+
+            grData.map(dtgr => {
+
+                if (isNaN(dtgr)) { dtgr = 0 };
+                if (dtgr === 0) { var dtgrprint = "" } else { var dtgrprint = dtgr };
+
+                if (dtgr < 10) {
                     doc
-                        .fontSize(8)
                         .fillColor("black")
-                        .text(dtgrprint, xxx+5, 660)
-                xxx = xxx+ 40;
-            }) 
+                        .text(dtgrprint, xxx + 10, 390)
+                } else if (dtgr > 10 & dtgr <= 999) {
+                    doc
+                        .fillColor("black")
+                        .text(dtgrprint, xxx + 8, 390)
+                } else {
+                    doc
+                        .fillColor("black")
+                        .text(dtgrprint, xxx + 5, 390)
+                }
+
+                if (dtgr > 0) {
+                    doc
+                        .rect(xxx + 10, 450, 20, dtgr * 200 / dtgraphMax).fillAndStroke("#efebe9", "#d7ccc8")
+                } else {
+                    doc
+                        .rect(xxx + 10, 650, 20, 0).fillAndStroke("#efebe9", "#d7ccc8")
+                }
+
+                xxx = xxx + 40;
+                doc
+                    .moveTo(xxx - 5, 375).lineTo(xxx - 5, 395).lineWidth(1).stroke("black")
+                    .moveTo(xxx - 20, 652).lineTo(xxx - 20, 657).lineWidth(1).stroke("black")
+
+            })
+            var xxx = 60;
+            grDay.map(dtgr => {
+
+                if (dtgr === 0) { var dtgrprint = "" } else { var dtgrprint = new Date(dtgr).toLocaleDateString() };
+                doc
+                    .fontSize(8)
+                    .fillColor("black")
+                    .text(dtgrprint, xxx + 5, 660)
+                xxx = xxx + 40;
+            })
         });
     });
 
@@ -178,78 +173,74 @@ function balanceLoad(req) {
     })
 }
 
-function dataGraph (devparam, balance) {
+function dataGraph(devparam, balance) {
 
     return new Promise(resolve => {
-    var prouts = [0];
+        var prouts = [0];
 
-    LocPrint
-        .find({ device: devparam, year: new Date().getFullYear() }, { printouts: 1, month: 1, _id: 0 }).sort({ date: 1 }).limit(12)
-        .exec(function (err, device) {
-            if (err) {
-                console.log(err)
-            } else {
+        LocPrint
+            .find({ device: devparam, year: new Date().getFullYear() }, { printouts: 1, month: 1, _id: 0 }).sort({ date: 1 }).limit(12)
+            .exec(function (err, device) {
+                if (err) {
+                    console.log(err)
+                } else {
 
-                var months = device.map(item => item.month);
-                var maxMonth = Math.max.apply(null, months);
-                var firstMonth = Math.min.apply(null, months);
+                    var months = device.map(item => item.month);
+                    var maxMonth = Math.max.apply(null, months);
+                    var firstMonth = Math.min.apply(null, months);
 
-                var arr01 = device.map(item => item.printouts);
-                var k = maxMonth - arr01.length;
+                    var arr01 = device.map(item => item.printouts);
+                    var k = maxMonth - arr01.length;
 
-                var bal1 = balance.map(item => item.balance);
+                    var bal1 = balance.map(item => item.balance);
 
-                for (var j = 0; j < k; j++) {
-                    var t = arr01.unshift(0);
-                };
+                    for (var j = 0; j < k; j++) {
+                        var t = arr01.unshift(0);
+                    };
 
-                for (var i = 1; i <= maxMonth; i++) {
-                    if (i === firstMonth - 1 && firstMonth > 0) {
-                        var val = arr01[i] - bal1[0];
-                    } else {
-                        var val = arr01[i] - arr01[i - 1];
+                    for (var i = 1; i <= maxMonth; i++) {
+                        if (i === firstMonth - 1 && firstMonth > 0) {
+                            var val = arr01[i] - bal1[0];
+                        } else {
+                            var val = arr01[i] - arr01[i - 1];
+                        }
+                        prouts.push(val);
                     }
-                    prouts.push(val);
+                    resolve(prouts);
                 }
-                resolve(prouts);
-            }
-        });
-})};
+            });
+    })
+};
 
-function dateGraph (devparam, balance) {
+function dateGraph(devparam, balance) {
 
     return new Promise(resolve => {
-    var prouts = [0];
+        var prouts = [0];
 
-    LocPrint
-        .find({ device: devparam, year: new Date().getFullYear() }, { date: 1, month: 1, _id: 0 }).sort({ date: 1 }).limit(12)
-        .exec(function (err, device) {
-            if (err) {
-                console.log(err)
-            } else {
+        LocPrint
+            .find({ device: devparam, year: new Date().getFullYear() }, { date: 1, month: 1, _id: 0 }).sort({ date: 1 }).limit(12)
+            .exec(function (err, device) {
+                if (err) {
+                    console.log(err)
+                } else {
 
-                var months = device.map(item => item.month);
-                var maxMonth = Math.max.apply(null, months);
-                var firstMonth = Math.min.apply(null, months);
+                    var months = device.map(item => item.month);
+                    var maxMonth = Math.max.apply(null, months);
+                    var firstMonth = Math.min.apply(null, months);
 
-                var arr01 = device.map(item => item.date);
-                var k = maxMonth - arr01.length;
+                    var arr01 = device.map(item => item.date);
+                    var k = maxMonth - arr01.length;
 
-                //var bal1 = balance.map(item => item.balance);
+                    for (var j = 0; j < k; j++) {
+                        var t = arr01.unshift(0);
+                    };
 
-                for (var j = 0; j < k; j++) {
-                    var t = arr01.unshift(0);
-                };
-
-                for (var i = 1; i <= maxMonth; i++) {
-                   // if (i === firstMonth - 1 && firstMonth > 0) {
+                    for (var i = 1; i <= maxMonth; i++) {
                         var val = arr01[i];
-                    //} else {
-                        //var val = arr01[i] - arr01[i - 1];
-                    //}
-                    prouts.push(val);
+                        prouts.push(val);
+                    }
+                    resolve(prouts);
                 }
-                resolve(prouts);
-            }
-        });
-})};
+            });
+    })
+};
